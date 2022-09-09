f07fda1776dde7a2d935ed1c8c8df12d9a24b986
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-09-09 11:51:55 +0700

    HashedPasswordV3

--- src/Practical.AspNetCore.Identity/Practical.AspNetCore.Identity.PasswordHasher/PasswordHasherExtensions.cs
+++ src/Practical.AspNetCore.Identity/Practical.AspNetCore.Identity.PasswordHasher/PasswordHasherExtensions.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Security.Cryptography;
 
 namespace AspNetCore.Identity.Extensions
 {
     public static class PasswordHasherExtensions
     {
+        public static HashDetails? ParseHashedPassword<TUser>(this IPasswordHasher<TUser> passwordHasher, string hashedPassword) where TUser : class
+        {
+            return ParseHashedPassword(Convert.FromBase64String(hashedPassword));
+        }
+
+        private static HashDetails? ParseHashedPassword(byte[] hashedPassword)
+        {
+            if (hashedPassword[0] == 0)
+                return ParseHashedPasswordV2(hashedPassword);
+            if (hashedPassword[0] == 1)
+                return ParseHashedPasswordV3(hashedPassword);
+            return ParseHashedPasswordV3(hashedPassword);
+        }
+
         public static HashDetails? ParseHashedPasswordV2<TUser>(this IPasswordHasher<TUser> passwordHasher, string hashedPassword) where TUser : class
         {
             return ParseHashedPasswordV2(Convert.FromBase64String(hashedPassword));
@@ -33,6 +46,7 @@ namespace AspNetCore.Identity.Extensions
 
             return new HashDetails
             {
+                Version = hashedPassword[0],
                 SaltBytes = salt,
                 HashBytes = expectedSubkey,
                 SaltString = Convert.ToBase64String(salt),
@@ -46,10 +60,86 @@ namespace AspNetCore.Identity.Extensions
         {
             return Convert.ToBase64String(new Rfc2898DeriveBytes(password, salt, 1000, HashAlgorithmName.SHA1).GetBytes(32));
         }
+
+        public static HashDetails? ParseHashedPasswordV3<TUser>(this IPasswordHasher<TUser> passwordHasher, string hashedPassword) where TUser : class
+        {
+            return ParseHashedPasswordV3(Convert.FromBase64String(hashedPassword));
+        }
+
+        private static HashDetails? ParseHashedPasswordV3(byte[] hashedPassword)
+        {
+            try
+            {
+                // Read header information
+                KeyDerivationPrf prf = (KeyDerivationPrf)ReadNetworkByteOrder(hashedPassword, 1);
+                int iterCount = (int)ReadNetworkByteOrder(hashedPassword, 5);
+                int saltLength = (int)ReadNetworkByteOrder(hashedPassword, 9);
+
+                // Read the salt: must be >= 128 bits
+                if (saltLength < 128 / 8)
+                {
+                    return null;
+                }
+                byte[] salt = new byte[saltLength];
+                Buffer.BlockCopy(hashedPassword, 13, salt, 0, salt.Length);
+
+                // Read the subkey (the rest of the payload): must be >= 128 bits
+                int subkeyLength = hashedPassword.Length - 13 - salt.Length;
+                if (subkeyLength < 128 / 8)
+                {
+                    return null;
+                }
+                byte[] expectedSubkey = new byte[subkeyLength];
+                Buffer.BlockCopy(hashedPassword, 13 + salt.Length, expectedSubkey, 0, expectedSubkey.Length);
+
+                return new HashDetails
+                {
+                    Version = hashedPassword[0],
+                    SaltBytes = salt,
+                    HashBytes = expectedSubkey,
+                    SaltString = Convert.ToBase64String(salt),
+                    HashString = Convert.ToBase64String(expectedSubkey),
+                    Interations = iterCount,
+                    KeyDerivationPrf = prf
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string HashPasswordV3<TUser>(this IPasswordHasher<TUser> passwordHasher, string password, byte[] salt, int iterations, KeyDerivationPrf keyDerivationPrf) where TUser : class
+        {
+            var hashAlgorithmName = HashAlgorithmName.SHA1;
+            switch (keyDerivationPrf)
+            {
+                case KeyDerivationPrf.HMACSHA1:
+                    hashAlgorithmName = HashAlgorithmName.SHA1;
+                    break;
+                case KeyDerivationPrf.HMACSHA256:
+                    hashAlgorithmName = HashAlgorithmName.SHA256;
+                    break;
+                case KeyDerivationPrf.HMACSHA512:
+                    hashAlgorithmName = HashAlgorithmName.SHA512;
+                    break;
+            }
+            return Convert.ToBase64String(new Rfc2898DeriveBytes(password, salt, iterations, hashAlgorithmName).GetBytes(32));
+        }
+
+        private static uint ReadNetworkByteOrder(byte[] buffer, int offset)
+        {
+            return ((uint)(buffer[offset + 0]) << 24)
+                | ((uint)(buffer[offset + 1]) << 16)
+                | ((uint)(buffer[offset + 2]) << 8)
+                | ((uint)(buffer[offset + 3]));
+        }
     }
 
     public class HashDetails
     {
+        public int Version { get; set; }
+
         public byte[]? SaltBytes { get; set; }
 
         public byte[]? HashBytes { get; set; }
--- src/Practical.AspNetCore.Identity/Practical.AspNetCore.Identity.PasswordHasher/Program.cs
+++ src/Practical.AspNetCore.Identity/Practical.AspNetCore.Identity.PasswordHasher/Program.cs
@@ -47,6 +47,52 @@ namespace Practical.AspNetCore.Identity.PasswordHasher
             return VerifyHashedPasswordV2(decodedHashedPassword, password);
         }
 
+        private static bool VerifyHashedPasswordV3(byte[] hashedPassword, string password)
+        {
+            try
+            {
+                // Read header information
+                KeyDerivationPrf prf = (KeyDerivationPrf)ReadNetworkByteOrder(hashedPassword, 1);
+                int iterCount = (int)ReadNetworkByteOrder(hashedPassword, 5);
+                int saltLength = (int)ReadNetworkByteOrder(hashedPassword, 9);
+
+                // Read the salt: must be >= 128 bits
+                if (saltLength < 128 / 8)
+                {
+                    return false;
+                }
+                byte[] salt = new byte[saltLength];
+                Buffer.BlockCopy(hashedPassword, 13, salt, 0, salt.Length);
+
+                // Read the subkey (the rest of the payload): must be >= 128 bits
+                int subkeyLength = hashedPassword.Length - 13 - salt.Length;
+                if (subkeyLength < 128 / 8)
+                {
+                    return false;
+                }
+                byte[] expectedSubkey = new byte[subkeyLength];
+                Buffer.BlockCopy(hashedPassword, 13 + salt.Length, expectedSubkey, 0, expectedSubkey.Length);
+
+                // Hash the incoming password and verify it
+                byte[] actualSubkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, subkeyLength);
+
+                return CryptographicOperations.FixedTimeEquals(actualSubkey, expectedSubkey);
+            }
+            catch
+            {
+                // This should never occur except in the case of a malformed payload, where
+                // we might go off the end of the array. Regardless, a malformed payload
+                // implies verification failed.
+                return false;
+            }
+        }
+
+        private static bool VerifyHashedPasswordV3(string hashedPassword, string password)
+        {
+            byte[] decodedHashedPassword = Convert.FromBase64String(hashedPassword);
+            return VerifyHashedPasswordV3(decodedHashedPassword, password);
+        }
+
         private static bool ByteArraysEqual(byte[] a, byte[] b)
         {
             if (a == null && b == null)
@@ -65,7 +111,21 @@ namespace Practical.AspNetCore.Identity.PasswordHasher
             return areSame;
         }
 
+        private static uint ReadNetworkByteOrder(byte[] buffer, int offset)
+        {
+            return ((uint)(buffer[offset + 0]) << 24)
+                | ((uint)(buffer[offset + 1]) << 16)
+                | ((uint)(buffer[offset + 2]) << 8)
+                | ((uint)(buffer[offset + 3]));
+        }
+
         static void Main(string[] args)
+        {
+            V2();
+            V3();
+        }
+
+        private static void V2()
         {
             var passwordHasher = new PasswordHasher<object>(Options.Create(new PasswordHasherOptions { CompatibilityMode = PasswordHasherCompatibilityMode.IdentityV2 }));
             var password1 = passwordHasher.HashPassword(null, "phongtest123");
@@ -85,6 +145,34 @@ namespace Practical.AspNetCore.Identity.PasswordHasher
 
             var hashedPassword11 = passwordHasher.HashPasswordV2("phongtest123", parsed1.SaltBytes);
             var hashedPassword22 = passwordHasher.HashPasswordV2("phongtest123", parsed2.SaltBytes);
+
+            var parsed3 = passwordHasher.ParseHashedPassword(hashedPassword1);
+            var parsed4 = passwordHasher.ParseHashedPassword(hashedPassword2);
+        }
+
+        private static void V3()
+        {
+            var passwordHasher = new PasswordHasher<object>(Options.Create(new PasswordHasherOptions { CompatibilityMode = PasswordHasherCompatibilityMode.IdentityV3 }));
+            var password1 = passwordHasher.HashPassword(null, "phongtest123");
+            var password2 = passwordHasher.HashPassword(null, "phongtest123");
+
+            var hashedPassword1 = "AQAAAAEAACcQAAAAEK8BtdVA98QGEu7kORnXMTUf1JfuXdqcmqzG1PkK9QVRhrUmwnsK+SoYppOnnFDFDQ==";
+            var hashedPassword2 = "AQAAAAEAACcQAAAAEDoO/1NMGyKMYovHoyoPntKPJoH4IGsLZjUIHAXktwA6iA+jGuzDB1ceR3zE+ipZsg==";
+
+            var check1 = passwordHasher.VerifyHashedPassword(null, hashedPassword1, "phongtest123");
+            var check2 = passwordHasher.VerifyHashedPassword(null, hashedPassword2, "phongtest123");
+
+            var check3 = VerifyHashedPasswordV3(hashedPassword1, "phongtest123");
+            var check4 = VerifyHashedPasswordV3(hashedPassword2, "phongtest123");
+
+            var parsed1 = passwordHasher.ParseHashedPasswordV3(hashedPassword1);
+            var parsed2 = passwordHasher.ParseHashedPasswordV3(hashedPassword2);
+
+            var hashedPassword11 = passwordHasher.HashPasswordV3("phongtest123", parsed1.SaltBytes, parsed1.Interations, parsed1.KeyDerivationPrf);
+            var hashedPassword22 = passwordHasher.HashPasswordV3("phongtest123", parsed2.SaltBytes, parsed2.Interations, parsed2.KeyDerivationPrf);
+
+            var parsed3 = passwordHasher.ParseHashedPassword(hashedPassword1);
+            var parsed4 = passwordHasher.ParseHashedPassword(hashedPassword2);
         }
     }
 }
